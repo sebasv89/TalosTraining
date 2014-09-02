f51cc01dff5dc755adb997171e796420eba5d47f
Jesse Cogollo <cogollo87@gmail.com>
2014-09-02 15:11:51 -0500

    codigo ebook java

---
+++ ebookJava/src/codeEbook/Asignacion.java
@@ -0,0 +1,16 @@
+package codeEbook;
+
+public class Asignacion {
+	int year;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Asignacion as = new Asignacion();
+	    as.showYear();
+	}
+	
+	public void showYear() {
+	      System.out.println("The year is " + year);
+	}
+
+}
